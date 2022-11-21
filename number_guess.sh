9aca568f7f942febcd2396e6622a7905799fb0b6
LaPlanta01 <101958129+LaPlanta01@users.noreply.github.com>
2022-11-21 18:37:36 -0300

    Update number_guess.sh

--- number_guess.sh
+++ number_guess.sh
@@ -4,10 +4,12 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 NUMBER=$(($RANDOM%1000+1))
 
 USERNAME_F (){
-	read -p "Enter your username: " USERNAME
+	echo -e "\nEnter your username:" 
+	read USERNAME
+
 	if [[ ${#USERNAME} -lt 22 ]]
 	then
-		echo Username has to be at least of 22 characters
+		echo -e "\nUsername has to be at least of 22 characters"
 		USERNAME_F
 	fi
 
@@ -21,14 +23,14 @@ USERNAME_F (){
 			READ_USER=$($PSQL "SELECT username, games_played, best_game FROM users WHERE username = '$USERNAME'")
 			echo $READ_USER | while IFS="|" read USERNAME GAMES_PLAYED BEST_GAME
 		do
-			echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+			echo -e "\nWelcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 		done
 		fi
 	done
 	if [[ $COINCIDENCE != 1 ]]
 	then
 		INSERT_NEW_USER=$($PSQL "INSERT INTO users (username, games_played, best_game) VALUES ('$USERNAME', 0, 0)")			
-		echo "Welcome, $USERNAME! It looks like this is your first time here."
+		echo -e "\nWelcome, $USERNAME! It looks like this is your first time here."
 	fi
 
 			
@@ -36,18 +38,24 @@ USERNAME_F (){
 
 GUESSING_NUMBER (){
 
-	read -p "Guess the secret number between 1 and 1000: " INPUT_NUMBER
+	echo -e "\nGuess the secret number between 1 and 1000: " 
+	read INPUT_NUMBER
+
 	while [[ $INPUT_NUMBER -ne $NUMBER ]]
 	do
-		if [[ $INPUT_NUMBER > $NUMBER ]]
+		if [[ ! $INPUT_NUMBER =~ ^[0-9]+$ ]]
+		then 
+			echo -e "\nThat is not an integer, guess again: " 
+			read INPUT_NUMBER
+		elif [[ $INPUT_NUMBER > $NUMBER ]]
 		then
-			read -p "It's lower than that, guess again: " INPUT_NUMBER
+			echo -e "\nIt's lower than that, guess again: " 
+			read INPUT_NUMBER
 		elif [[ $INPUT_NUMBER < $NUMBER ]]
 		then 
-			read -p "It's higher than that, guess again: " INPUT_NUMBER
-		elif [[ ! $INPUT_NUMBER =~ (^[0-9]+$) ]]
-		then 
-			read -p "That is not an integer, guess again: " INPUT_NUMBER
+			echo -e "\nIt's higher than that, guess again: " 
+			read INPUT_NUMBER
+
 		fi
 
 		NUMBER_OF_GUESSES=$(( $NUMBER_OF_GUESSES + 1 ))
@@ -55,7 +63,7 @@ GUESSING_NUMBER (){
 
 	done
 	
-	echo "You guessed it in $NUMBER_OF_GUESSES tries. The secret number was $NUMBER. Nice job!"
+	echo -e "\nYou guessed it in $NUMBER_OF_GUESSES tries. The secret number was $NUMBER. Nice job!"
 
 	return $NUMBER_OF_GUESSES
 
